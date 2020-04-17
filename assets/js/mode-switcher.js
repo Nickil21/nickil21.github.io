2cc213ec0ffd908f5d558541caf377b157075df6
Nickil Maveli <nickil21jhum@gmail.com>
2020-04-17 13:53:10 +0530

    update

--- assets/js/mode-switcher.js
+++ assets/js/mode-switcher.js
@@ -2,19 +2,19 @@ let systemInitiatedDark = window.matchMedia("(prefers-color-scheme: dark)");
 let theme = sessionStorage.getItem('theme');
 
 if (systemInitiatedDark.matches) {
-	document.getElementById("theme-toggle").innerHTML = "☀";
+	document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-sun\"></i>';
 } else {
-	document.getElementById("theme-toggle").innerHTML = "☾";
+	document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-moon\" style="color: grey"></i>';
 }
 
 function prefersColorTest(systemInitiatedDark) {
   if (systemInitiatedDark.matches) {
   	document.documentElement.setAttribute('data-theme', 'dark');
-   	document.getElementById("theme-toggle").innerHTML = "☀";
+   	document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-sun\"></i>';
    	sessionStorage.setItem('theme', '');
   } else {
   	document.documentElement.setAttribute('data-theme', 'light');
-    document.getElementById("theme-toggle").innerHTML = "☾";
+    document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-moon\" style="color: grey"></i>';
     sessionStorage.setItem('theme', '');
   }
 }
@@ -26,30 +26,30 @@ function modeSwitcher() {
 	if (theme === "dark") {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
-		document.getElementById("theme-toggle").innerHTML = "☾";
+		document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-moon\" style="color: grey"></i>';
 	}	else if (theme === "light") {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "☀";
+		document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-sun\"></i>';
 	} else if (systemInitiatedDark.matches) {
 		document.documentElement.setAttribute('data-theme', 'light');
 		sessionStorage.setItem('theme', 'light');
 		//let theme = sessionStorage.getItem('theme');
 		//console.log("this was triggered");
-		document.getElementById("theme-toggle").innerHTML = "☾";
+		document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-moon\" style="color: grey"></i>';
 	} else {
 		document.documentElement.setAttribute('data-theme', 'dark');
 		sessionStorage.setItem('theme', 'dark');
-		document.getElementById("theme-toggle").innerHTML = "☀";
+		document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-sun\"></i>';
 	}
 }
 
 if (theme === "dark") {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	sessionStorage.setItem('theme', 'dark');
-	document.getElementById("theme-toggle").innerHTML = "☀";
+	document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-sun\"></i>';
 } else if (theme === "light") {
 	document.documentElement.setAttribute('data-theme', 'light');
 	sessionStorage.setItem('theme', 'light');
-	document.getElementById("theme-toggle").innerHTML = "☾";
+	document.getElementById("theme-toggle").innerHTML = '<i class=\"fas fa-moon\" style="color: grey"></i>';
 }
